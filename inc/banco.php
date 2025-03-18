68414d844997ee72a71e9bb6aa301ac248862c76
Gustavo Rossi da Luz <rossidaluzgustavo@gmail.com>
2025-03-18 08:46:22 -0300

    aula 3 - listagem de compras

---
+++ inc/banco.php
@@ -0,0 +1,9 @@
+<?php
+
+# inc/banco.php
+
+//lembrete: não se deve salvar senhas diretamente no arquivo (da maneira como estamos fazendo aqui)
+
+$pdo = new PDO('mysql:host=localhost;dbname=3info', 'root', '');
+
+
