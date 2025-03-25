fc046f7de26adb707517a39891f868bd8371c6cf
Gustavo Rossi da Luz <rossidaluzgustavo@gmail.com>
2025-03-25 08:47:49 -0300

    Carbon

---
+++ carbon_exemplos.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('vendor/autoload.php');
+date_default_timezone_set('America/Sao_Paulo');
+
+
+use Carbon\Carbon;
+
+echo Carbon::now() . '<br>';
+
+echo Carbon::now()->addDay() . '<br>';
+
+echo Carbon::now()->subWeek() . '<br>';
+
+echo 'Próximas Olimpíadas ' . Carbon::createFromDate(2024)->addYears(4)->year . '<br>';
+
+echo 'Sua idade é: ' . Carbon::createFromDate(2008, 1, 21)->age . '<br>';
+
+if(Carbon::now()->isWeekend()){
+    echo 'Festa!!!';
+} else {
+    echo 'Aula :(';
+}
+
+echo '<br>';
+
+$nascimento = Carbon::createFromDate(2008, 1, 21);
+echo 'Diferença de data ' . Carbon::now()->diff($nascimento);
---
+++ horario.php
@@ -0,0 +1,13 @@
+<?php
+
+require('twig_carregar.php');
+require_once('vendor/autoload.php');
+date_default_timezone_set('America/Sao_Paulo');
+
+use Carbon\Carbon;
+
+$horario = Carbon::now();
+
+echo $twig->render('horario.html', [
+    'horario' => $horario,
+]);
