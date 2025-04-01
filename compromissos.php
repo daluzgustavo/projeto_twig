54387fcc212315de804c89ada758c0716411d9ef
Gustavo Rossi da Luz <rossidaluzgustavo@gmail.com>
2025-04-01 08:40:09 -0300

    sla

--- compromissos.php
+++ compromissos.php
@@ -7,10 +7,10 @@ date_default_timezone_set('America/Sao_Paulo');
 
 use Carbon\Carbon;
 
-$ordem == $_GET['ordem'] ?? null;
+$ordem = $_GET['ordem'] ?? null;
 
 if ($ordem == 'mais_proximo'){
-    $dados = $pdo->query('SELECT * FROM compromissos ORDER BY data ASC WHERE GETDATE() <= data');
+    $dados = $pdo->query('SELECT * FROM compromissos  WHERE NOW() <= data ORDER BY data ASC');
     
     $compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
 
@@ -20,12 +20,12 @@ if ($ordem == 'mais_proximo'){
         $compromisso['final_de_semana'] =  $final_de_semana;
     }
 
-    echo $twig->render('compromissos.html', [
-        'titulo' => 'Compromissos',
-        'compromissos' => $compromissos,
-    ]);
+    // echo $twig->render('compromissos.html', [
+    //     'titulo' => 'Compromissos',
+    //     'compromissos' => $compromissos,
+    // ]);
 } else if ($ordem == 'mais_distante'){
-    $dados = $pdo->query('SELECT * FROM compromissos ORDER BY data DESC WHERE GETDATE() <= data');
+    $dados = $pdo->query('SELECT * FROM compromissos WHERE NOW() <= data ORDER BY data DESC ');
     
     $compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
 
@@ -35,20 +35,10 @@ if ($ordem == 'mais_proximo'){
         $compromisso['final_de_semana'] =  $final_de_semana;
     }
 
-    echo $twig->render('compromissos.html', [
-        'titulo' => 'Compromissos',
-        'compromissos' => $compromissos,
-    ]);
-}
-
-$dados = $pdo->query('SELECT * FROM compromissos');
-
-$compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($compromissos as &$compromisso){
-    $data = Carbon::parse($compromisso['data']);
-    $final_de_semana = Carbon::createFromDate($data)->isWeekend();
-    $compromisso['final_de_semana'] =  $final_de_semana;
+//     echo $twig->render('compromissos.html', [
+//         'titulo' => 'Compromissos',
+//         'compromissos' => $compromissos,
+//     ]);
 }
 
 echo $twig->render('compromissos.html', [
